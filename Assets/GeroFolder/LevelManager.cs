51d66e6747ea01297f9c0752a26fa0b86a4b2e2c
geropython <72422102+geropython@users.noreply.github.com>
2023-06-13 16:02:53 -0300

    cosas UX finalizadas

--- Assets/GeroFolder/LevelManager.cs
+++ Assets/GeroFolder/LevelManager.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,12 +6,12 @@ public class LevelManager : MonoBehaviour
     public void StartGame()
     {
         //Juego
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
     public void MainMenu()
     {
         //Menu
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
     public void QuitGame()
     {
